070bd0998c1e2ebc6b7766f983a6abe8d66fd974
myx <2726232435@qq.com>
2020-08-03 12:09:51 +0800

    修改错题

--- homework.js
+++ homework.js
@@ -22,21 +22,50 @@
 
 // const compose = fp.flowRight(_average,fp.map(v=>v.dollar_value));  
 //--4
-const fp = require('lodash/fp')
-const _underscore = fp.replace(/\W+/g, '_');
-const format = (v) => [v]
+// const fp = require('lodash/fp')
+// const _underscore = fp.replace(/\W+/g, '_');
+// const format = (v) => [v]
 
-const sanitizeNames = fp.flowRight(format, fp.toLower, _underscore, fp.first);
-console.log(sanitizeNames(["Hello World"]))
+// const sanitizeNames = fp.flowRight(format, fp.toLower, _underscore, fp.first);
+// console.log(sanitizeNames(["Hello World"]))
+
+
+
+class Container {
+    static of(v){
+        return new Container(v)
+    }
+    constructor(v){
+        this._value = v;
+    }
+    map(fn){
+        return Container.of(fn(this._value))
+    }
+}
+class Maybe {
+    static of(v){
+        return new Maybe(v)
+    }
+    isNothing() {
+        return this._value === null||this._value===undefined
+    }
+    constructor(v){
+        this._value = v;
+    }
+    map(fn){
+        return this.isNothing()?this: Maybe.of(fn(this._value))
+    }
+}
 
 //三--1
-// const fp = require('lodash/fp')
-
-// const ex1 = () => {
-//     const container = Container.of(1)
-//     const newContainer =  container.map(fp.add(1))
-//     const newMaybe = maybe.map(fp.map(fp.add(1)))
-// }
+const fp = require('lodash/fp')
+const maybe = Maybe.of([5,6,1])
+const ex1 = (x) => {
+    const container = Container.of(1)
+    const newContainer =  container.map(fp.add(1))
+    const newMaybe = maybe.map(fp.map(fp.add(x))).map(console.log)
+}
+// ex1(2)
 
 //--2
 
@@ -45,12 +74,20 @@ const ex2 = ()=>{
 }
 
 //--3
+const safeProp = fp.curry((o,x)=>Maybe.of(o[x]));
+const user = {id:2,name:'Albert'}
 const ex3 = ()=>{
-    const functor = safeProp(user,name);
-    functor.map(fp.flowRight(fp.first,fp.split('')));
+    const functor = safeProp(user,'name');
+    console.log(functor)
+    functor.map(fp.flowRight(fp.first,fp.split(''))).map(console.log);
 }
+ex3()
 
 //--4
 const ex4 = n=>{
     return Maybe.of(n).map(parseInt);
 }
+
+
+
+
